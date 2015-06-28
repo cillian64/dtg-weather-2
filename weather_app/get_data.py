459190d516ac5bce42119ea1d4bbf6962e205a61
David Turner <cillian64@googlemail.com>
2015-06-28 01:20:47 +0100

    Added historic API, renamed API fields

--- weather_app/get_data.py
+++ weather_app/get_data.py
@@ -1,8 +1,21 @@
 import psycopg2 as pg
 
-inst_sensors = ["temp", "hum", "dewpt", "pressure", "windspd", "winddir",
-                "sunshine", "rainfall"]
-hist_sensors = []
+inst_sensors = {"temperature": "insttemp",
+                "humidity": "insthum",
+                "dewpoint": "instdewpt",
+                "pressure": "instpressure",
+                "windspeed": "instwindspd",
+                "winddirection": "instwinddir",
+                "sunshine": "instsunshine",
+                "rainfall": "instrainfall"}
+hist_sensors = {"temperature": "avtemp",
+                "humidity": "avhum",
+                "dewpoint": "avdewpt",
+                "pressure": "avpressure",
+                "windspeed": "avwindspd",
+                "winddirection": "avwinddir",
+                "sunshine": "instsunhours",
+                "rainfall": "instrainfall"}
 
 def all_sensors(db, datefrom, dateto, inst):
     """
@@ -12,8 +25,6 @@ def all_sensors(db, datefrom, dateto, inst):
     If inst is true, use few-secondly readings, if False, half-hourly
     Result looks like {'temp': tempdata, 'dewpt': dewptdata, ...}
     """
-    if not inst:
-        raise NotImplementedError
     sensors = inst_sensors if inst else hist_sensors
 
     results = dict()
@@ -32,36 +43,45 @@ def get_sensor(db, sensor, datefrom, dateto, inst):
     if datefrom is None or dateto is None:
         raise ValueError("datefrom and dateto must both be specified")
 
-    sensors = inst_sensors if inst else hist_sensors
-    if not inst:
-        raise NotImplementedError
+    if inst:
+        sensors = inst_sensors
+        table = "tblWeatherInstantaneous"
+    else:
+        sensors = hist_sensors
+        table = "tblWeatherHistoric"
 
     if sensor not in sensors:
         raise ValueError("Invalid sensor type '{}'".format(sensor))
 
     cur = db.cursor()
     RECORD_LIMIT = 50000
-    query = "SELECT timestamp, {} ".format("inst" + sensor)
-    query += "FROM tblWeatherInstantaneous "
+    query = "SELECT timestamp, {} ".format(sensors[sensor])
+    query += "FROM {} ".format(table)
     query += "WHERE timestamp BETWEEN %s and %s LIMIT %s"
     cur.execute(query, (datefrom, dateto, RECORD_LIMIT))
     
-    converter = convert(sensor)
+    converter = convert(sensors[sensor])
     return [(t, converter(data)) for (t, data) in cur]
 
 
-def convert(sensor):
+def convert(sensorfield):
     """
     Return a converter to convert data readings from database format to API
     format for a given sensor type.  i.e:
     data_api = convert("temp")(data_sql)
     """
-    if sensor in ["temp", "dewpt", "windspd"]:
+    if sensorfield in ["insttemp", "avtemp", "instdewpt", "avdewpt",
+                       "instwindspd", "avwindspd"]:
         return lambda data: 0.1 * data
-    elif sensor == "rainfall":
+    elif sensorfield == "instrainfall":
         return lambda data: 0.001 * data
-    elif sensor in ["winddir", "hum", "sunshine", "pressure"]:
+    elif sensorfield == "instsunshine":  # From inst
+        return lambda data: data/3600.0  # Stored in seconds, conv to hours
+    elif sensorfield == "instsunhours":  # From hist
+        return lambda data: data/100.0  # Stored in hundredths of hours
+    elif sensorfield in ["instwinddir", "avwinddir", "insthum", "avhum",
+                        "instpressure", "avpressure"]:
         return lambda data: data
     else:
-        raise ValueError("Invalid sensor type '{}'".format(sensor))
+        raise ValueError("Invalid sensor type '{}'".format(sensorfield))
 
